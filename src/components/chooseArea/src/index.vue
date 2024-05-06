dfee92c8c51df290fc6aaa9f712db8c55ecb44c6
unicornlee <unicornlee@dingtalk.com>
2024-05-06 21:28:53 +0800

    2-12省市区选择组件-完善省市区联动组件并给父组件分发事件

--- src/components/chooseArea/src/index.vue
+++ src/components/chooseArea/src/index.vue
@@ -1,24 +1,85 @@
 <template>
   <div class="flex flex-wrap gap-4 items-center">
-    <el-select size="large" style="width: 240px" placeholder="请选择省份" v-model="province">
-      <el-option value="1">1</el-option>
+    <el-select clearable size="large" style="width: 240px" placeholder="请选择省份" v-model="province">
+      <el-option v-for="item in areas" :key="item.code" :value="item.code" :label="item.name"></el-option>
     </el-select>
-    <el-select size="large" style="width: 240px; margin: 0 10px;" placeholder="请选择城市" v-model="city">
-      <el-option value="2">2</el-option>
+    <el-select clearable :disabled="!province" size="large" style="width: 240px; margin: 0 10px;" placeholder="请选择城市" v-model="city">
+      <el-option v-for="item in selectCity" :key="item.code" :value="item.code" :label="item.name"></el-option>
     </el-select>
-    <el-select size="large" style="width: 240px" placeholder="请选择区域" v-model="area">
-      <el-option value="3">3</el-option>
+    <el-select clearable :disabled="!province || !city" size="large" style="width: 240px" placeholder="请选择区域" v-model="area">
+      <el-option v-for="item in selectArea" :key="item.code" :value="item.code" :label="item.name"></el-option>
     </el-select>
   </div>
 </template>
 
 <script setup lang="ts">
-import {ref} from 'vue'
+import {ref, watch} from 'vue'
 import allAreas from '../lib/pca-code.json'
-console.log(allAreas)
+
+export interface AreaItem {
+  code: string,
+  name: string,
+  children: AreaItem[]
+}
+
+export interface Data {
+  name: string,
+  code: string
+}
+
+// 下拉框选择省份的值
 let province = ref<string>('')
+// 下拉框选择城市的值
 let city = ref<string>('')
+// 下拉框选择区域的值
 let area = ref<string>('')
+// 所有的省市区数据
+let areas = ref(allAreas)
+
+// 城市下拉框的所有的值
+let selectCity = ref<AreaItem[]>([])
+
+watch(() => province.value, val => {
+  if (val) {
+    selectCity.value = areas.value.find(item => item.code === val)?.children || []
+  }
+  city.value = ''
+  area.value = ''
+})
+
+// 区域下拉框的所有的值
+let selectArea = ref<AreaItem[]>([])
+
+watch(() => city.value, val => {
+  if (val) {
+    selectArea.value = selectCity.value?.find(item => item.code === val)?.children || []
+  }
+  area.value = ''
+})
+
+const emits = defineEmits(['change'])
+// 监听选择区域的值
+watch(() => area.value, val => {
+  if (val) {
+    let provinceData: Data = {
+      code: province.value,
+      name: allAreas.find(item => item.code === province.value)?.name || ''
+    }
+    let cityData: Data = {
+      code: city.value,
+      name: selectCity.value.find(item => item.code === city.value)?.name || ''
+    }
+    let areaData: Data = {
+      code: val,
+      name: selectArea.value.find(item => item.code === val)?.name || ''
+    }
+    emits('change', {
+      province: provinceData,
+      city: cityData,
+      area: areaData
+    })
+  }
+})
 </script>
 
 <style scoped lang="scss">
--- src/views/chooseArea/index.vue
+++ src/views/chooseArea/index.vue
@@ -1,9 +1,13 @@
 <template>
-  <m-choose-area />
+  <m-choose-area @change="changeArea" />
 </template>
 
 <script setup lang="ts">
 import mChooseArea from '../../components/chooseArea/src/index.vue'
+
+const changeArea = (area: string) => {
+  console.log(area)
+}
 </script>
 
 <style scoped lang="scss">
