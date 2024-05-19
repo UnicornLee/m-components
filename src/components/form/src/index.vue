a19674a9cbcc0bd64a34820c75b8e02ca282ac0b
UnicornLee <1571418313@qq.com>
2024-05-19 21:20:10 +0800

    5-5表单组件-巧用component动态组件配置添加子元素组件

--- src/components/form/src/index.vue
+++ src/components/form/src/index.vue
@@ -1,8 +1,20 @@
 <template>
-  <el-form :validate-on-rule-change="false" v-bind="$attrs" :model="model" :rules="rules">
-    <el-form-item
-      v-for="(item, index) in options"
-      :key="index"
+  <el-form v-if="model" :validate-on-rule-change="false" v-bind="$attrs" :model="model" :rules="rules">
+    <template v-for="(item, index) in options" :key="index">
+      <el-form-item
+        v-if="!item.children || !item.children.length"
+        :label="item.label"
+        :prop="item.prop"
+        :rules="item.rules"
+      >
+        <component
+          v-bind="item.attrs"
+          :is="`el-${item.type}`"
+          v-model="model[item.prop!]"
+        />
+      </el-form-item>
+      <el-form-item
+      v-if="item.children && item.children.length"
       :label="item.label"
       :prop="item.prop"
       :rules="item.rules"
@@ -11,13 +23,23 @@
         v-bind="item.attrs"
         :is="`el-${item.type}`"
         v-model="model[item.prop!]"
-      />
+      >
+        <component
+          v-for="(child, i) in item.children"
+          :key="i"
+          :is="`el-${child.type}`"
+          :label="child.label"
+          :value="child.value"
+        />
+      </component>
     </el-form-item>
+
+    </template>
   </el-form>
 </template>
 
 <script setup lang="ts">
-import { onMounted, PropType, ref } from 'vue'
+import { onMounted, PropType, ref, watch } from 'vue'
 import { FormOptions } from './types/types'
 import cloneDeep from 'lodash/cloneDeep'
 
@@ -29,20 +51,31 @@ let props = defineProps({
   }
 })
 
-let model = ref<any>({})
-let rules = ref<any>({})
+let model = ref<any>(null)
+let rules = ref<any>(null)
+
+// 初始化表单
+const initForm = () => {
+  if (props.options && props.options.length) {
+    let m: any = {}
+    let r: any = {}
+    props.options.forEach((item: FormOptions) => {
+      m[item.prop!] =item.value
+      r[item.prop!] = item.rules
+    })
+    model.value = cloneDeep(m)
+    rules.value = cloneDeep(r)
+  }
+}
 
 onMounted(() => {
-  let m: any = {}
-  let r: any = {}
-  props.options.forEach((item: FormOptions) => {
-    m[item.prop!] =item.value
-    r[item.prop!] = item.rules
-  })
-  model.value = cloneDeep(m)
-  rules.value = cloneDeep(r)
-  console.log(model.value, rules.value)
+  initForm()
 })
+
+// 监听父组件传递进来的options
+watch(() => props.options, () => {
+  initForm()
+}, { deep: true })
 </script>
 
 <style scoped lang="scss">
--- src/views/form/index.vue
+++ src/views/form/index.vue
@@ -8,7 +8,7 @@ import { FormOptions } from '../../components/form/src/types/types.ts'
 let options: FormOptions[] = [
   {
     type: 'input',
-    value: '',
+    value: 'admin',
     label: '用户名',
     prop: 'username',
     rules: [
@@ -30,7 +30,7 @@ let options: FormOptions[] = [
   },
   {
     type: 'input',
-    value: '',
+    value: '123456',
     label: '密码',
     prop: 'password',
     rules: [
@@ -50,6 +50,102 @@ let options: FormOptions[] = [
       showPassword: true,
       clearable: true
     }
+  },
+  {
+    type: 'select',
+    value: '1',
+    placeholder: '请选择职位',
+    prop: 'role',
+    label: '职位',
+    attrs: {
+      style: {
+        width: '100%'
+      }
+    },
+    rules: [
+      {
+        required: true,
+        message: '职位不能为空',
+        trigger: 'blur'
+      }
+    ],
+    children: [
+      {
+        type: 'option',
+        label: '经理',
+        value: '1'
+      },
+      {
+        type: 'option',
+        label: '主管',
+        value: '2'
+      },
+      {
+        type: 'option',
+        label: '员工',
+        value: '3'
+      }
+    ]
+  },
+  {
+    type: 'checkbox-group',
+    value: [],
+    prop: 'like',
+    label: '爱好',
+    rules: [
+      {
+        required: true,
+        message: '爱好不能为空',
+        trigger: 'blur'
+      }
+    ],
+    children: [
+      {
+        type: 'checkbox',
+        label: '足球',
+        value: '1'
+      },
+      {
+        type: 'checkbox',
+        label: '篮球',
+        value: '2'
+      },
+      {
+        type: 'checkbox',
+        label: '排球',
+        value: '3'
+      }
+    ]
+  },
+  {
+    type: 'radio-group',
+    value: [],
+    prop: 'gender',
+    label: '性别',
+    rules: [
+      {
+        required: true,
+        message: '性别不能为空',
+        trigger: 'blur'
+      }
+    ],
+    children: [
+      {
+        type: 'radio',
+        label: '男',
+        value: 'male'
+      },
+      {
+        type: 'radio',
+        label: '女',
+        value: 'female'
+      },
+      {
+        type: 'radio',
+        label: '保密',
+        value: 'unknown'
+      }
+    ]
   }
 ]
 </script>
